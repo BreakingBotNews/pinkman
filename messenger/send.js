b0d16afeba8258922d40f77e1b3befc911b700b6
Luca Hammer <lucahammer@gmail.com>
2016-09-28 16:28:09 +0200

    tried putting getSummary in send.js to find a bug. but failed.

--- messenger/send.js
+++ messenger/send.js
@@ -1,10 +1,25 @@
 var l = require('../utilities/logUtils');
 var db = require('../api/controllers/dbRequests');
 var sendMessage = require('../api/controllers/SendMessages');
+var axios = require('axios');
+
+
+function getSummary(sender, length, callback) {
+    console.log("Heisenberg, was ist der neue Shizzle?");
+    var obj = {
+        summaryRequest: {
+            length: length,
+            fbId: sender
+        }
+    };
+    axios.post(urlArticle,obj).then(function (result) {
+        callback(result);
+    })
+}
 
 function summary(target) {
     console.log("getting summary");
-    db.getSummary(target,5,function (result) {
+    getSummary(target,5,function (result) {
         result = result.data;
         console.log(result);
         for (var i=0; i<result.length; i++){
